3c2cbfea7a66a9edfbf153d745ce601c61fae251
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-02 12:57:15 +0300

    being more portable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-# master node
-multipass launch --name microk8s-vm1 --cpus 4 --mem 16G --disk 100G
-multipass exec microk8s-vm1 -- sudo snap install microk8s --classic --channel=1.18/stable
-multipass exec microk8s-vm1 -- sudo iptables -P FORWARD ACCEPT
-multipass exec microk8s-vm1 -- sudo microk8s status --wait-ready
-multipass exec microk8s-vm1 -- sudo microk8s enable dns storage metrics-server prometheus ingress dashboard jaeger
+echo "############################################"
+echo "###                                     ####"
+echo "### Initializing kubernetes cluster     ####"
+echo "###                                     ####"
+echo "############################################"
+
+. $CNLAB_HOME/.env_sh
+
+# create node using multipass, omit if not required
+[[ $create_node = "yes" ]] && multipass launch --name microk8s-vm1 --cpus 4 --mem 16G --disk 100G
+
+snap install microk8s --classic --channel=1.18/stable
+
+# Darwin specific accroding to documentation
+[[ $platform = "Darwin" ]] && multipass exec microk8s-vm1 -- sudo iptables -P FORWARD ACCEPT
+
+microk8s status --wait-ready
+microk8s enable dns storage metrics-server prometheus ingress dashboard jaeger
